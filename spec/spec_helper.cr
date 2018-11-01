ff76f2dc10c781e2337e095f010ddcaeba8a1ab2
Jack Thorne <jack@kilmer.co>
2018-11-01 14:09:33 -0500

    remove notes in spec helper

--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -2,14 +2,4 @@ require "spec"
 require "../src/socks"
 require "http/server"
 
-#PORT = rand(8000..10000)
-PORT = ENV["PORT"].to_i
-
-
 # `ssh -D 1080 -C -N 127.0.0.1`
-
-#pp! Process.new("ssh -D #{PORT} -C -N root@167.99.184.241")
-#pp! `ssh -D #{PORT} -C -N root@167.99.184.241`
-
-#sock = Socket.tcp(Socket::Family::INET6)
-#sock.bind Socket::IPAddress.new("127.0.0.1", PORT)
