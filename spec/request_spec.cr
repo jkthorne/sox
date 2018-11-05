339bf3414ca50d2e14b32afb0e48d6a2955482ac
Jack Thorne <jack@kilmer.co>
2018-11-05 00:48:46 -0600

    remove mutipility from requests

--- spec/request_spec.cr
+++ spec/request_spec.cr
@@ -2,89 +2,78 @@ require "./spec_helper"
 
 describe Socks::Request do
   it "default buffer" do
-    expected_buffer = Socks::Request::IPV4_BUFFER
+    expected_buffer = Bytes[
+      Socks::V5, Socks::COMMAND::CONNECT, 0_u8, Socks::ADDR_TYPE::IPV4,
+      0_u8, 0_u8, 0_u8, 0_u8, 0_u8, 0_u8
+    ]
 
     actual_request = Socks::Request.new(addr: "0.0.0.0", port: 0)
     
     actual_request.buffer.should eq expected_buffer
   end
 
-  it "initializes with values" do
-    expected_addr = "127.0.0.1"
-    expected_port = 80
+  describe "initializes" do
+    it "sets port" do
+      expected_addr = "127.0.0.1"
+      expected_port = 8888
 
-    actual_request = Socks::Request.new(addr: expected_addr, port: expected_port)
-    
-    actual_request.addr.should eq expected_addr
-    actual_request.port.should eq expected_port 
-  end
-
-  it "sets versions" do
-    expected_version = Socks::V5
-    actual_request = Socks::Request.new("127.0.0.1")
-    
-    actual_request.version.should eq expected_version
-  end
-
-  it "sets reply" do
-    expected_reply = Socks::COMMAND::BIND
-
-    actual_request = Socks::Request.new("127.0.0.1")
-    
-    actual_request.reply = expected_reply
-    actual_request.reply.should eq expected_reply
-
-    actual_request.reply = :connect
-    actual_request.reply.should eq Socks::COMMAND::CONNECT
-  end
-
-  it "sets addr_type" do
-    expected_addr_type = Socks::ADDR_TYPE::IPV6
-
-    actual_request = Socks::Request.new("127.0.0.1")
-
-    actual_request.addr_type = expected_addr_type
-    actual_request.addr_type.should eq expected_addr_type
-
-    actual_request.addr_type = :domain
-    actual_request.addr_type.should eq Socks::ADDR_TYPE::DOMAIN
-  end
-
-  describe "addr" do
-    it "sets ipv4 addr" do
-      expected_addr = "225.225.225.225"
-      actual_request = Socks::Request.new("::1")
-
-      actual_request.addr = expected_addr
+      actual_request = Socks::Request.new(addr: expected_addr, port: expected_port)
+      
       actual_request.addr.should eq expected_addr
+      actual_request.port.should eq expected_port
     end
 
-    it "sets ipv6 addr" do
-      expected_addr = "0:0:0:0:0:0:0:1"
-      expected_addr_alt = "::1"
-      actual_request = Socks::Request.new("127.0.0.1")
-
-      actual_request.addr = expected_addr
-      actual_request.addr.should eq expected_addr
-
-      actual_request.addr = expected_addr_alt
-      actual_request.addr.should eq expected_addr
+    it "sets versions" do
+      expected_version = Socks::V4
+      actual_request = Socks::Request.new("127.0.0.1", version: expected_version)
+      
+      actual_request.version.should eq expected_version
     end
 
-    it "sets domain addr" do
-      expected_addr = "www.example.com"
-      actual_request = Socks::Request.new("127.0.0.1")
+    it "sets reply" do
+      expected_reply = Socks::COMMAND::BIND
+      actual_request = Socks::Request.new("127.0.0.1", command: expected_reply)
 
-      actual_request.addr = expected_addr
-      actual_request.addr.should eq expected_addr
+      actual_request.reply.should eq expected_reply
     end
-  end
 
-  it "sets port" do
-    expected_port = 8888
-    actual_request = Socks::Request.new("127.0.0.1")
+    describe "addr" do
+      it "sets ipv4 addr" do
+        expected_addr = "225.225.225.225"
+        actual_request = Socks::Request.new(addr: expected_addr)
 
-    actual_request.port = expected_port
-    actual_request.port.should eq expected_port
+        actual_request.addr.should eq expected_addr
+        actual_request.addr_type.should eq Socks::ADDR_TYPE::IPV4
+      end
+
+      it "sets ipv6 addr" do
+        expected_addr = "0:0:0:0:0:0:0:1"
+        actual_request = Socks::Request.new(addr: expected_addr)
+
+        actual_request.addr.should eq expected_addr
+        actual_request.addr_type.should eq Socks::ADDR_TYPE::IPV6
+      end
+
+      it "sets ipv6 shorts" do
+        actual_request = Socks::Request.new(addr: "::1")
+
+        actual_request.addr.should eq "0:0:0:0:0:0:0:1"
+        actual_request.addr_type.should eq Socks::ADDR_TYPE::IPV6
+      end
+
+      it "sets domain addr" do
+        expected_addr = "www.example.com"
+        actual_request = Socks::Request.new(addr: expected_addr)
+
+        actual_request.addr.should eq expected_addr
+        actual_request.addr_type.should eq Socks::ADDR_TYPE::DOMAIN
+      end
+    end
+
+    it "raises with invalid addr" do
+      expect_raises(Socket::Error) do
+        Socks::Request.new(addr: "!@#$")
+      end
+    end
   end
 end
--- src/request.cr
+++ src/request.cr
@@ -1,64 +1,55 @@
 class Socks::Request
-  IPV4_BUFFER = Bytes[
-    V5, COMMAND::CONNECT, RESERVED, ADDR_TYPE::IPV4, BLANK_BYTE,
-    BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE
-  ]
-  IPV6_BUFFER = Bytes[
-    V5, COMMAND::CONNECT, RESERVED, ADDR_TYPE::IPV6, BLANK_BYTE,
-    BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE,
-    BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE,
-    BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE, BLANK_BYTE,
-    BLANK_BYTE, BLANK_BYTE
-  ]
-  DEFAULT_BUFFER = Bytes[V5, COMMAND::CONNECT, RESERVED, ADDR_TYPE::DOMAIN, BLANK_BYTE, BLANK_BYTE]
-
   property buffer : Bytes
 
-  def initialize(addr : String , port : Int = 80)
-    case IPAddress.new(addr, port).family
-    when Family::INET
-      @buffer = IPV4_BUFFER.clone
-    when Family::INET6
-      @buffer = IPV6_BUFFER.clone
+  def initialize(addr : String , port : Int = 80, version : UInt8 = V5, command : UInt8 = COMMAND::CONNECT)
+    if addr.scan(/[a-zA-Z]/).size > 0
+      @buffer = Bytes.new(addr.size + 6)
+      @buffer[3] = ADDR_TYPE::DOMAIN
+      addr.to_slice.copy_to(@buffer[4, addr.to_slice.size])
     else
-      @buffer = DEFAULT_BUFFER.clone
+      ip_address = Socket::IPAddress.new(addr, port)
+      case ip_address.family
+      when Family::INET
+        @buffer = Bytes.new(10)
+        @buffer[3] = ADDR_TYPE::IPV4
+        ip_address.address.split(".").each_with_index { |b, i|
+          @buffer[4 + i] = b.to_u8
+        }
+      when Family::INET6
+        @buffer = Bytes.new(22)
+        @buffer[3] = ADDR_TYPE::IPV6
+        {% if flag?(:darwin) || flag?(:openbsd) || flag?(:freebsd) %}
+          ip_address.@addr6.not_nil!.__u6_addr.__u6_addr8.to_slice.copy_to @buffer[4, 16]
+        {% elsif flag?(:linux) && flag?(:musl) %}
+          ip_address.@addr6.not_nil!.__in6_union.__s6_addr.to_slice.copy_to @buffer[4, 16]
+        {% elsif flag?(:linux) %}
+          ip_address.@addr6.not_nil!.__in6_u.__u6_addr8.to_slice.copy_to @buffer[4, 16]
+        {% else %}
+          {% raise "Unsupported platform" %}
+        {% end %}
+      else
+        raise "invalid addr type for Socks::Request"
+      end
     end
-    self.addr = addr
-    self.port = port
-  end
 
-  def version=(version : Int)
-    buffer[0] = version.to_u8
-    version
+    ## Set Socks version
+    @buffer.[0] = version
+
+    ## Set Socks command
+    @buffer.[1] = command
+
+    ## Set port
+    IO::ByteFormat::NetworkEndian.encode(
+      port.to_u16, @buffer.[@buffer.size - 2, 2].to_slice
+    )
   end
 
   def version
     buffer[0]
   end
 
-  def reply=(command : Symbol)
-    case command
-    when :connect
-      buffer[1] = COMMAND::CONNECT
-    when :bind
-      buffer[1] = COMMAND::BIND
-    when :udp
-      buffer[1] = COMMAND::UDP_ASSOCIATE
-    end
-    reply
-  end
-
-  def reply=(command : UInt8)
-    buffer[1] = command
-  end
-
   def reply
     buffer[1]
-  end
-
-  def addr_type=(addr_type : UInt8)
-    buffer[3] = addr_type
-    addr_type
   end
 
   def addr_type=(addr_type new_addr_type : Symbol = :ipv4)
@@ -75,43 +66,6 @@ class Socks::Request
 
   def addr_type
     buffer[3]
-  end
-
-  def addr=(addr new_addr : String)
-    if new_addr.scan(/[a-zA-Z]/).size > 0
-      new_buffer = Bytes.new(new_addr.size + 6)
-
-      buffer[0, 2].copy_to new_buffer[0, 2]                                 ## copy meta data
-      new_buffer[3] = ADDR_TYPE::DOMAIN                                     ## set addr type
-      new_addr.to_slice.copy_to(new_buffer[4, new_addr.to_slice.size])      ## copy domain
-      buffer[buffer.size - 2, 2].copy_to new_buffer[new_buffer.size - 2, 2] ## copy ports
-
-      @buffer = new_buffer
-      return addr
-    end
-
-    ip_address = Socket::IPAddress.new(new_addr, port.to_i32)
-
-    case ip_address.family
-    when Family::INET
-      alter_buffer(ADDR_TYPE::IPV4)
-      ip_address.address.split(".").each_with_index { |b, i|
-        buffer[4 + i] = b.to_u8
-      }
-    when Family::INET6
-      alter_buffer(ADDR_TYPE::IPV6)
-      {% if flag?(:darwin) || flag?(:openbsd) || flag?(:freebsd) %}
-        ip_address.@addr6.not_nil!.__u6_addr.__u6_addr8.to_slice.copy_to buffer[4, 16]
-      {% elsif flag?(:linux) && flag?(:musl) %}
-        ip_address.@addr6.not_nil!.__in6_union.__s6_addr.to_slice.copy_to buffer[4, 16]
-      {% elsif flag?(:linux) %}
-        ip_address.@addr6.not_nil!.__in6_u.__u6_addr8.to_slice.copy_to buffer[4, 16]
-      {% else %}
-        {% raise "Unsupported platform" %}
-      {% end %}
-    end
-
-    addr
   end
 
   def addr
@@ -131,12 +85,6 @@ class Socks::Request
     end
   end
 
-  def port=(port_number : Int)
-    port_buffer = buffer[buffer.size - 2, 2].to_slice
-    IO::ByteFormat::NetworkEndian.encode(port_number.to_u16, port_buffer)
-    port
-  end
-
   def port
     port_buffer = buffer[buffer.size - 2, 2].to_slice
     IO::ByteFormat::NetworkEndian.decode(Int16, port_buffer)
@@ -149,20 +97,5 @@ class Socks::Request
   def inspect(io)
     io << "#<Socks::Request version=#{version} reply=#{reply} "
     io << "addr_type=#{addr_type} addr=#{addr} port=#{port}>"
-  end
-
-  private def alter_buffer(new_addr_type : UInt8)
-    case new_addr_type
-    when ADDR_TYPE::IPV4
-      new_buffer = IPV4_BUFFER.clone
-      buffer[0, 2].copy_to new_buffer[0, 2]
-      buffer[buffer.size - 2, 2].copy_to new_buffer[new_buffer.size - 2, 2]
-      @buffer = new_buffer
-    when ADDR_TYPE::IPV6
-      new_buffer = IPV6_BUFFER.clone
-      buffer[0, 2].copy_to new_buffer[0, 2]
-      buffer[buffer.size - 2, 2].copy_to new_buffer[new_buffer.size - 2, 2]
-      @buffer = new_buffer
-    end
   end
 end
